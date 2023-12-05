8a5d020bf6e94eabb10459c273d5e2207d621dea
Adam Sanchez <722945@student.dusd.net>
2023-12-05 14:26:07 -0800

    added the awake and start script
    added and finished the awake and start assignment, adding it to a cube.

--- ScriptingProjectSanchez/Assets/Scripts/AwakeAndStart.cs
+++ ScriptingProjectSanchez/Assets/Scripts/AwakeAndStart.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class AwakeAndStart : MonoBehaviour
 {
+    void Awake()
+    {
+        Debug.Log("Awake called."); 
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start called.");
     }
 
     // Update is called once per frame
